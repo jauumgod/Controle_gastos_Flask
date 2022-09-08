fa541fc55491b1e6e5b008c0078bc520897aeb50
jauumgod <69704112+jauumgod@users.noreply.github.com>
2022-09-08 10:19:18 -0300

    Update controle.py

--- controle.py
+++ controle.py
@@ -9,12 +9,13 @@ class controle:
     self.app.geometry("800x600")
     self.app.config(bg="MIDNIGHTBLUE")
     
-    label1 = Label(self.app, text="Controle", bg="MIDNIGHTBLUE", fg="WHITE")
+    label1 = Label(self.app, text="Controle", bg="MIDNIGHTBLUE", fg="WHITE", font="arial bold 18")
     label1.place(x=350, y=100)
     
     entrada1 = Entry(self.app, widht=25)
     entrada1.place(x=350,y=150)
     
+    label2 = Label(self.app, text="Cartôes", bg="MIDNIGHTBLUE")
     
     
     
