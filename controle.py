4b948705b19223bd56395fd56411abd8ff9433b3
jauumgod <69704112+jauumgod@users.noreply.github.com>
2022-09-08 10:14:28 -0300

    Update controle.py

--- controle.py
+++ controle.py
@@ -12,6 +12,9 @@ class controle:
     label1 = Label(self.app, text="Controle", bg="MIDNIGHTBLUE", fg="WHITE")
     label1.place(x=350, y=100)
     
+    entrada1 = Entry(self.app, widht=25)
+    entrada1.place(x=350,y=150)
+    
     
     
     
