5a2e85985cbdd75842e43b21fc623e876b8a34ea
jauumgod <69704112+jauumgod@users.noreply.github.com>
2022-09-08 10:13:35 -0300

    Create controle.py

---
+++ controle.py
@@ -0,0 +1,37 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+
+
+class controle:
+  def __init__(self):
+    self.app = Tk()
+    self.app.geometry("800x600")
+    self.app.config(bg="MIDNIGHTBLUE")
+    
+    label1 = Label(self.app, text="Controle", bg="MIDNIGHTBLUE", fg="WHITE")
+    label1.place(x=350, y=100)
+    
+    
+    
+    
+    
+    
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    self.app.mainlopp()
+controle()
